3302d21b6c0f72465f9bee39d219f6ac3ffe2359
Sabari Yuhendhran M <159673631+sabariyuhendh@users.noreply.github.com>
2024-07-03 13:38:57 +0530

    scripts.js

--- scripts.js
+++ scripts.js
@@ -5,3 +5,10 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+function openPdf() {
+  // Specify the path to your PDF file
+  var pdfPath = 'path/to/your/pdf/document.pdf';
+  
+  // Open the PDF in a new window/tab
+  window.open(pdfPath, '_blank');
+}
