6c74abff27888e00980740f5063dd57676f8ad91
Sabari Yuhendhran M <159673631+sabariyuhendh@users.noreply.github.com>
2024-07-09 09:06:54 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,29 +2,6 @@ function openPdf() {
   var pdfPath = '/Resume-1.pdf';
   window.open(pdfPath, '_blank');
 }
-<div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-  <div className="flex flex-col">
-    <span className="countdown font-mono text-5xl">
-      <span style={{"--value":15}}></span>
-    </span>
-    days
-  </div>
-  <div className="flex flex-col">
-    <span className="countdown font-mono text-5xl">
-      <span style={{"--value":10}}></span>
-    </span>
-    hours
-  </div>
-  <div className="flex flex-col">
-    <span className="countdown font-mono text-5xl">
-      <span style={{"--value":24}}></span>
-    </span>
-    min
-  </div>
-  <div className="flex flex-col">
-    <span className="countdown font-mono text-5xl">
-      <span style={{"--value":${counter}}}></span>
-    </span>
-    sec
-  </div>
-</div>
+function(){
+  window.open('about.html', '_self');
+}
